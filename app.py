050d31373d7be3d4f249257c8252ed0e9b0c986b
User <kd200681@mail.ru>
2023-08-11 17:05:01 +0300

    Mode admin and user

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from aiogram import types
-from aiogram.types import ReplyKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+from data.config import ADMINS
 from loader import dp
 
 user_message = 'Пользователь'
@@ -19,7 +21,7 @@ async def cmd_start(message: types.Message):
 
     await message.answer('''Привет! 👋
 
-🤖 Я бот-магазин по подаже товаров любой категории.
+🤖 Я бот-магазин по подаже товаров одежды бренда Luvvy.
 
 🛍️ Чтобы перейти в каталог и выбрать приглянувшиеся 
 товары возпользуйтесь командой /menu.
@@ -27,3 +29,28 @@ async def cmd_start(message: types.Message):
 ❓ Возникли вопросы? Не проблема! Команда /sos поможет 
 связаться с админами, которые постараются как можно быстрее откликнуться.
     ''', reply_markup=markup)
+
+
+@dp.message_handler(text=admin_message)
+async def admin_mode(message: types.Message):
+    """Выбор пользователем режима администратора"""
+    cid = message.chat.id
+    if cid not in ADMINS:
+        await message.answer('Извините, Вас нет в списке админов')
+        # ADMINS.append(cid)
+
+    # ReplyKeyboardRemove() удаляем клавиатуру выбора режима
+    await message.answer('Включен админский режим.',
+                         reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(text=user_message)
+async def user_mode(message: types.Message):
+    """Выбор пользователем режима пользователя"""
+    # cid = message.chat.id
+    # if cid in ADMINS:
+    #     ADMINS.remove(cid)
+
+    await message.answer('Включен пользовательский режим.',
+                         reply_markup=ReplyKeyboardRemove())
+
